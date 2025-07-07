09dab2889ca691bbd330d1eeb7a97e0d3163b3ad
kj <kahjun1018@gmail.com>
2025-07-07 11:17:41 +0800

    feat: add example for gsap.timeline() usage

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -1,5 +1,39 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
+  const timeline = gsap.timeline({
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 1,
+  });
+
+  useGSAP(() => {
+    timeline.to("#yellow-box", {
+      x: 250,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
+
+    timeline.to("#yellow-box", {
+      y: 250,
+      scale: 2,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
+
+    timeline.to("#yellow-box", {
+      x: 500,
+      rotation: 180,
+      borderRadius: "10%",
+      duration: 1,
+      ease: "power1.inOut",
+    });
+  }, []);
 
   return (
     <main>
@@ -35,7 +69,17 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button
+          onClick={() => {
+            if (timeline.paused()) {
+              timeline.play();
+            } else {
+              timeline.pause();
+            }
+          }}
+        >
+          Play/Pause
+        </button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
