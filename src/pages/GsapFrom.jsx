17f9451812d5d60f8ab8f1ff2692f7195b8cef61
kj <kahjun1018@gmail.com>
2025-07-07 11:16:41 +0800

    feat: add example for gsap.from() animation

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 600,
+      duration: 4,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      ease: "elastic",
+    });
+  });
 
   return (
     <main>
